d37989d3f538937ab8804274e202bf820fed7b78
MikeTheShadow <miketheshadow1@gmail.com>
2020-04-30 12:16:52 -0600

    Updated API other minor changes

--- src/main/java/com/miketheshadow/complexproficiencies/api/UserAPI.java
+++ src/main/java/com/miketheshadow/complexproficiencies/api/UserAPI.java
@@ -5,6 +5,8 @@ import com.miketheshadow.complexproficiencies.utils.DBHandlers.UserDBHandler;
 import me.realized.duels.api.user.User;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+
 public class UserAPI {
 
     @Deprecated
@@ -13,7 +15,7 @@ public class UserAPI {
     }
 
     public static boolean userHasLabor(Player player,int amount) {
-        return UserDBHandler.getPlayer(player).getLabor() > amount;
+        return UserDBHandler.getPlayer(player).getLabor() >= amount;
     }
 
     public static void updateUserProf(Player player, String prof, int amount) {
@@ -22,5 +24,27 @@ public class UserAPI {
 
     public static void updateUser(CustomUser user){
         UserDBHandler.updatePlayer(user);
+    }
+
+    public static int getProfLevel(Player player,String profname) {
+        CustomUser user = UserDBHandler.getPlayer(player);
+        if(user.getProfessions() == null) return -1;
+        return user.getLevelFromProf(profname);
+    }
+
+    public static HashMap<String, Integer> getAllProfs(Player player) {
+        CustomUser user = UserDBHandler.getPlayer(player);
+        if(user.getProfessions() == null) return null;
+        return user.getProfessions();
+    }
+
+    public static void addExperienceToProf(Player player,String profname,int experienceAmount) {
+        CustomUser user = UserDBHandler.getPlayer(player);
+        user.addExperience(profname,experienceAmount,player);
+        UserDBHandler.updatePlayer(user);
+    }
+
+    public static int getUserLabor(Player player) {
+        return UserDBHandler.getPlayer(player).getLabor();
     }
 }
