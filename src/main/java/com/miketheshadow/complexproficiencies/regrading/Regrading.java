4eeb9a9dd398e384c75752102346d2e0569253e0
miketheshadow <miketheshadow1@gmail.com>
2020-07-17 12:47:23 -0600

    Added license

--- src/main/java/com/miketheshadow/complexproficiencies/regrading/Regrading.java
+++ src/main/java/com/miketheshadow/complexproficiencies/regrading/Regrading.java
@@ -1,16 +1,28 @@
+/*
+ *
+ *  * Copyright 2019-2020 Michael Pape and contributors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 package com.miketheshadow.complexproficiencies.regrading;
 
-import de.tr7zw.nbtapi.NBTContainer;
 import de.tr7zw.nbtapi.NBTItem;
-import net.milkbowl.vault.chat.Chat;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Regrading {
 
