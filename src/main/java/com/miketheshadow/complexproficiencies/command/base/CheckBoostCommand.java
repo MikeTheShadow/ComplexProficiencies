a2d5fff898dfb1ff1713ce42a113a8ebbde3c1e6
TechnoWizzy <TechnoWizzy@users.noreply.github.com>
2020-12-12 15:15:53 -0500

    Create CheckBoostCommand.java

---
+++ src/main/java/com/miketheshadow/complexproficiencies/command/base/CheckBoostCommand.java
@@ -0,0 +1,25 @@
+package com.miketheshadow.complexproficiencies.command.base;
+
+import com.miketheshadow.complexproficiencies.ComplexProficiencies;
+import com.miketheshadow.complexproficiencies.command.ComplexCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CheckBoostCommand extends ComplexCommand {
+
+    public CheckBoostCommand() {
+        super("checkboost");
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull org.bukkit.command.Command cmd, @NotNull String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("createbooster")) {
+            double multiplier = ComplexProficiencies.xpMultiplier;
+            sender.sendMessage(ChatColor.GREEN + "Current Boost: " + ChatColor.WHITE +  multiplier + "x");
+            return true;
+        }
+        return false;
+    }
+}
